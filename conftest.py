77c86cd245a0f8aefc9296f4bf059fa6c6a35b7e
Sarah Hsu <sarah@gigatms.com.tw>
2025-10-29 18:20:54 +0800

    Test(TDD): Add failing tests for DataFetcher

---
+++ conftest.py
@@ -0,0 +1,7 @@
+# 檔案: conftest.py
+import sys
+import os
+
+# (關鍵) 將 'src' 目錄添加到 Python 的模組搜索路徑中 (list 的最前面)
+# 這讓我們在 tests/ 裡的測試檔可以寫: 'from data_fetcher import ...'
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'src')))
